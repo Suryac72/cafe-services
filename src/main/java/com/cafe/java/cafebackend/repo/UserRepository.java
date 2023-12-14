42da0f28c9c66fc0c15978cdee1db86a8a4cac23
surya <suryac72@gmail.com>
2023-12-14 21:49:01 +0530

    feat(initial-commit)

--- src/main/java/com/cafe/java/cafebackend/repo/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.cafe.java.cafebackend.repo;
-
-import java.util.UUID;
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.cafe.java.cafebackend.models.UserProfile;
-
-public interface UserRepository extends JpaRepository<UserProfile,UUID>{
-    UserProfile findByUserEmail(String userEmail);
-} 
--- src/main/java/com/cafe/java/cafebackend/services/env/EnvUtil.java
+++
@@ -1,8 +0,0 @@
-package com.cafe.java.cafebackend.services.env;
-
-import org.springframework.boot.context.properties.ConfigurationProperties;
-
-@ConfigurationProperties("cafe")
-public record EnvUtil(String secret) {
-    
-} 
