0998e627363968684daef85acaad561b00788bb7
surya <suryac72@gmail.com>
2023-12-14 22:46:04 +0530

    adding docker file

--- src/test/java/com/ecommerce/java/ecommercebackend/EcommerceBackendApplicationTests.java
+++ src/test/java/com/ecommerce/java/ecommercebackend/EcommerceBackendApplicationTests.java
@@ -2,12 +2,10 @@ package com.ecommerce.java.ecommercebackend;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.ComponentScan;
 
-@SpringBootTest
+@SpringBootTest(classes = EcommerceBackendApplicationTests.class)
+@ComponentScan(basePackages = "com.ecommerce.java.ecommercebackend")
 class EcommerceBackendApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
 
 }
